369d938db3e4e913df72e89d50e923c124fedcaf
LLeeJr <legralisandro@gmail.com>
2021-01-18 15:41:14 +0100

    location will be shown

--- app/src/main/java/com/group1/movebetter/MainActivity.kt
+++ app/src/main/java/com/group1/movebetter/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.group1.movebetter
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.LocationComponentOptions
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
+import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
 
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
-    private var locationOverlay: MyLocationNewOverlay? = null
-    private var mapView: com.mapbox.mapboxsdk.maps.MapView? = null
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListener {
+    private var mapView: MapView? = null
+    private var mapboxMap: MapboxMap? = null
+    private var permissionsManager: PermissionsManager? = null
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,24 +32,69 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         mapView = findViewById(R.id.mapView)
         mapView?.onCreate(savedInstanceState)
-        mapView?.getMapAsync { mapboxMap ->
+        mapView?.getMapAsync(this)
 
-            mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+        }
 
-// Map is set up and the style has loaded. Now you can add data or make other map adjustments
+    override fun onMapReady(mapboxMap: MapboxMap) {
+        this.mapboxMap = mapboxMap
 
+        mapboxMap.setStyle(
+            Style.MAPBOX_STREETS
+        ) { enableLocationComponent(it) }
+    }
 
+    @SuppressLint("MissingPermission")
+    private fun enableLocationComponent(loadedMapStyle: Style) {
+// Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+
+// Create and customize the LocationComponent's options
+            val customLocationComponentOptions = LocationComponentOptions.builder(this)
+                .trackingGesturesManagement(true)
+                .accuracyColor(ContextCompat.getColor(this, R.color.mapboxGreen))
+                .build()
+
+            val locationComponentActivationOptions = LocationComponentActivationOptions.builder(this, loadedMapStyle)
+                .locationComponentOptions(customLocationComponentOptions)
+                .build()
+
+// Get an instance of the LocationComponent and then adjust its settings
+            mapboxMap!!.locationComponent.apply {
+
+// Activate the LocationComponent with options
+                activateLocationComponent(locationComponentActivationOptions)
+
+// Enable to make the LocationComponent visible
+                isLocationComponentEnabled = true
+
+// Set the LocationComponent's camera mode
+                cameraMode = CameraMode.TRACKING
+
+// Set the LocationComponent's render mode
+                renderMode = RenderMode.COMPASS
             }
-
+        } else {
+            permissionsManager = PermissionsManager(this)
+            permissionsManager!!.requestLocationPermissions(this)
         }
     }
 
-    override fun onMapReady(mapboxMap: MapboxMap) {
-        this@LocationComponentActivity.mapboxMap = mapboxMap
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        permissionsManager!!.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
 
-        mapboxMap.setStyle(
-            Style.Builder().fromUri("mapbox://styles/mapbox/cjerxnqt3cgvp2rmyuxbeqme7")
-        ) { style -> enableLocationComponent(style) }
+    override fun onExplanationNeeded(permissionsToExplain: List<String>) {
+        Toast.makeText(this, "R.string.user_location_permission_explanation", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted) {
+            enableLocationComponent(mapboxMap!!.style!!)
+        } else {
+            Toast.makeText(this, "R.string.user_location_permission_not_granted", Toast.LENGTH_LONG).show()
+            finish()
+        }
     }
 
     public override fun onResume() {
