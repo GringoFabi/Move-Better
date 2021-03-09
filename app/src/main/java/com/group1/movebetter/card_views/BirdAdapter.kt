7e9352f2dffd279c01658bb9bb972a3657369440
LLeeJr <legralisandro@gmail.com>
2021-03-09 16:53:45 +0100

    added bird app icon to card view

--- app/src/main/java/com/group1/movebetter/card_views/BirdAdapter.kt
+++ app/src/main/java/com/group1/movebetter/card_views/BirdAdapter.kt
@@ -1,12 +1,15 @@
 package com.group1.movebetter.card_views
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.text.SpannableString
 import android.text.style.UnderlineSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.cardview.widget.CardView
@@ -16,11 +19,13 @@ import com.mapbox.geojson.Feature
 
 class BirdAdapter(private val data: ArrayList<Feature>, private val openBirdApp: () -> Unit, private val navigateTo: (Double, Double) -> Unit) : RecyclerView.Adapter<BirdAdapter.BirdViewHolder?>() {
 
+    private lateinit var birdIconBitmap: Bitmap
     lateinit var context: Context
 
     class BirdViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var cv: CardView
         var title: TextView
+        var birdAppIcon: ImageView
         var estimatedRange: TextView
         var batteryLevelPB: ProgressBar
         var batteryLevel: TextView
@@ -29,7 +34,8 @@ class BirdAdapter(private val data: ArrayList<Feature>, private val openBirdApp:
 
         init {
             cv = itemView.findViewById(R.id.birdCardView)
-            title = itemView.findViewById(R.id.birdTitel)
+            title = itemView.findViewById(R.id.birdTitle)
+            birdAppIcon = itemView.findViewById(R.id.birdAppIcon)
             estimatedRange = itemView.findViewById(R.id.estimatedRange)
             batteryLevelPB = itemView.findViewById(R.id.batteryLevelPB)
             batteryLevel = itemView.findViewById(R.id.batteryLevel)
@@ -40,6 +46,7 @@ class BirdAdapter(private val data: ArrayList<Feature>, private val openBirdApp:
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BirdViewHolder {
         context = parent.context
+        birdIconBitmap = BitmapFactory.decodeResource(context.resources, R.raw.bird_app_icon)
         val v = LayoutInflater.from(parent.context).inflate(R.layout.card_view_birds, parent, false)
         return BirdViewHolder(v)
     }
@@ -48,6 +55,9 @@ class BirdAdapter(private val data: ArrayList<Feature>, private val openBirdApp:
         val content = SpannableString(data[position].getStringProperty("vehicleClass"))
         content.setSpan(UnderlineSpan(), 0, content.length, 0)
         holder.title.text = content
+        holder.birdAppIcon.setImageBitmap(birdIconBitmap)
+        holder.birdAppIcon.scaleType = ImageView.ScaleType.CENTER
+        holder.birdAppIcon.adjustViewBounds = true
 
         holder.estimatedRange.text = "Estimated Range = ${data[position].getNumberProperty("estimatedRange")}"
         holder.batteryLevelPB.progress = (data[position].getNumberProperty("batteryLevel") as Double).toInt()
